87917f2b1623bd429b94f23d68b1422df1506255
praveen0216 <praveen.kr.sharma02@gmail.com>
2023-07-27 11:17:30 +0530

    split method of Pattern

---
+++ src/tutorial/java/regularexpression/PatternSplitMethodDemo.java
@@ -0,0 +1,25 @@
+package tutorial.java.regularexpression;
+
+
+import java.util.regex.Pattern;
+
+public class PatternSplitMethodDemo {
+
+    private static final String  TARGET_STRING = "Praveen Kumar Sharma";
+    private static final String  TARGET_WEBSITE = "www.praveenk.com";
+
+    public static void main(String[] args) {
+
+        Pattern p =  Pattern.compile("\\s") ;
+        String[] split = p.split(TARGET_STRING);
+        for (String str : split) {
+            System.out.println(str);
+        }
+
+        Pattern dot =  Pattern.compile("[.]") ; // [.] or \\. 
+        String[] dots = dot.split(TARGET_WEBSITE);
+        for (String str : dots) {
+            System.out.println(str);
+        }
+    }
+}
