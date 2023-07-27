8eebedc445508962cb093fb187402e42d0bb334e
praveen0216 <praveen.kr.sharma02@gmail.com>
2023-07-27 18:41:31 +0530

    write mobile number to a file

---
+++ src/tutorial/java/regularexpression/MobileNumberExtractor.java
@@ -0,0 +1,25 @@
+package tutorial.java.regularexpression;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MobileNumberExtractor {
+
+    public static void main(String[] args) throws IOException {
+
+        PrintWriter pw = new PrintWriter("src/resource/output.txt");
+        Pattern p = Pattern.compile("(0|91)?[7-9][0-9]{9}");
+
+        BufferedReader br = new BufferedReader(new FileReader("src/resource/input.txt"));
+        String line = br.readLine();
+        while (line != null) {
+            Matcher m = p.matcher(line);
+            while (m.find()) {
+                pw.println(m.group());
+            }
+            line = br.readLine();
+        }
+        pw.flush();
+    }
+}
