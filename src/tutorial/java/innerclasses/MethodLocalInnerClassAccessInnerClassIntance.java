6f0bcf8f1b7b93365b8b9996194142b784c16344
praveen0216 <praveen.kr.sharma02@gmail.com>
2023-08-11 15:23:02 +0530

    method local inner class

---
+++ src/tutorial/java/innerclasses/MethodLocalInnerClassAccessInnerClassIntance.java
@@ -0,0 +1,21 @@
+package tutorial.java.innerclasses;
+
+public class MethodLocalInnerClassAccessInnerClassIntance {
+
+    public void m1() {
+        int x = 10;
+        //x=x+1;
+
+        class Inner {
+            public void m2() {
+                System.out.println(x); // if line 7 is uncommented : Variable 'x' is accessed from within inner class, needs to be final or effectively final
+            }
+        }
+        Inner i = new Inner();
+        i.m2();
+    }
+
+    public static void main(String[] args) {
+        new MethodLocalInnerClassAccessInnerClassIntance().m1();
+    }
+}
